81ca6f5589d11f7132f9dd03d208abdb117fbe17
Johanna Bogdanow <johanna.bogdanow@summitllc.us>
2022-09-26 15:09:08 -0400

    fixed the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,7 +13,13 @@ export default function Footer() {
             <a>How Search works</a>
           </li>
         </div>
-        <a>Carbon neutral since 2007</a>
+        <a>
+          <img
+            class="Leaf"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAYCAMAAAAiV0Z6AAAAPFBMVEVLoEN0wU6CzFKCzFKCzFKCzFKCzFJSo0MSczNDmkCCzFJPoUMTczNdr0gmgziCzFITczMTczMTczMTczPh00jOAAAAFHRSTlPF/+bIsms8Ad///hX+//5/tXw7aMEAx10AAACaSURBVHgBbc4HDoRQCATQ33tbvf9dF9QxaCT9UQaltLHOh/golXKhMs5Xqa0xU1lyoa2fXFyQOsDG38qsLy4TaV+sFislovyhPzLJJrBu6eQOtpW0LjbJkzTuTDLRVNKa3uxJI+VdiRqXSeu6GW+Qxi29eLIi8H7EsYrT42BD+mQtNO5JMjRuC4lSY8V4hsLX0egGijvUSEP9AbylEsOkeCgWAAAAAElFTkSuQmCC"
+          />
+          Carbon neutral since 2007
+        </a>
         <div class="FooterLinks">
           <li>
             <a>Privacy</a>
